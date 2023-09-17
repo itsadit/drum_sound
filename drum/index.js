85b9bd64393bfb13e16bbd144e17862fab293ae8
Aditya kumar <79790523+itsadit@users.noreply.github.com>
2023-09-17 21:31:46 +0530

    Add files via upload

---
+++ drum/index.js
@@ -0,0 +1,55 @@
+for(var i=0;i<7;i++)
+{
+document.querySelectorAll(".drum")[i].addEventListener("click",function () 
+{
+    var but=this.innerHTML;
+    makesound(but);
+    buttonanimation(but);// pressing effect
+}
+);
+}
+
+document.addEventListener( "keypress", function(event){ makesound(event.key);  buttonanimation(event.key)} );
+
+function makesound(key)// play sound based on the key got pressed
+{
+    switch(key){
+        case "w":
+            var audio1=new Audio("sounds/tom-1.mp3");
+            audio1.play();
+            break;
+        case "a":
+            var audio2=new Audio("sounds/tom-2.mp3");
+            audio2.play();
+            break;
+        case "s":
+            var audio3=new Audio("sounds/tom-3.mp3");
+            audio3.play();
+            break;
+        case "d":
+            var audio4=new Audio("sounds/tom-4.mp3");
+            audio4.play();
+            break;
+        case "j":
+            var audio5=new Audio("sounds/snare.mp3");
+            audio5.play();
+            break;
+        case "k":
+            var audio6=new Audio("sounds/crash.mp3");
+            audio6.play();
+            break;
+        case "l":
+            var audio7=new Audio("sounds/kick-bass.mp3");
+            audio7.play();
+            break;
+        default: console.log(but);
+    }
+}
+
+function buttonanimation(key)
+{
+    var activebutton=document.querySelector("."+key);
+    activebutton.classList.add("pressed");// adding pressed class for effect to the button got pressed
+
+    setTimeout(function() { activebutton.classList.remove("pressed"); },100);// to remove effect after 1 sec
+}
